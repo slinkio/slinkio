e955a8c00978d1dbe6bde9334ca3a59367870b86
James-Mac <james@aehr.org>
2014-06-19 15:17:08 -0600

    Move vendor css to app import

--- Brocfile.js
+++ Brocfile.js
@@ -14,19 +14,30 @@ var app = new EmberApp({
 });
 
 // Use this to add additional libraries to the generated output files.
-app.import({
-  development: 'vendor/ember-data/ember-data.js',
-  production:  'vendor/ember-data/ember-data.prod.js'
-});
+app.import(
+  {
+    development: 'vendor/ember-data/ember-data.js',
+    production:  'vendor/ember-data/ember-data.prod.js'
+  },
+  {
+    'ember-data': [
+      'default'
+    ]
+  }
+);
+/* Vendor CSS */
+app.import('vendor/fontawesome/css/font-awesome.css');
+app.import('vendor/nprogress/nprogress.css');
+
+/* Vendor JS */
+app.import('vendor/bootstrap/dist/js/bootstrap.min.js');
+app.import('vendor/nprogress/nprogress.js');
 
 // If the library that you are including contains AMD or ES6 modules that
 // you would like to import into your application please specify an
 // object with the list of modules as keys along with the exports of each
 // module as its value.
-app.import(
-  'vendor/bootstrap/dist/js/bootstrap.min.js',
-  'vendor/nprogress/nprogress.js',
-  'vendor/ic-ajax/dist/named-amd/main.js', {
+app.import('vendor/ic-ajax/dist/named-amd/main.js', {
     'ic-ajax': [
       'default',
       'defineFixture',
