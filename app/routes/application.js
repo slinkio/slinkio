614b6bd88fdffd8e087da4cc5dcf22092f3c1c0f
James-Mac <james@aehr.org>
2014-06-30 15:39:43 -0600

    Modal global action handler

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,27 @@
 import Ember from "ember";
 
 var ApplicationRoute = Ember.Route.extend({
+	// Define Global Action Handlers
+	actions: {
+		showModal: function (id, staticModal, forceAppend) {
+			// Assign the modal element to a variable
+			var el = $("#" + id);
+			// If the forceAppend variable exists, we will append it to that identifer; useful for nested view modals
+			if(forceAppend) {
+				// Reassign the element
+				el = el.appendTo(forceAppend);
+			}
+			// If we are going to be rendering this as a static, non-dismissable modal, set those properties
+			if(staticModal) {
+				el.modal({
+  					keyboard: false,
+					backdrop: 'static'
+				});
+			}
+			// Show the modal
+			el.modal('show');
+		}
+	}
 });
 
 export default ApplicationRoute;
